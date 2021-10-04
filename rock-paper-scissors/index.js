7c2075b3726aa74084b77c039e218cc12dca8f13
Jimmie <jimmieriss@gmail.com>
2021-10-04 09:53:22 +0200

    Random word generator for rock project is done.

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,5 +1,14 @@
 // Make a random output 
-
+function computerPlay(){
+    let randomNumber = Math.floor(Math.random() * 8);
+    if (randomNumber < 3) {
+        return "Rock";
+    } else if (randomNumber < 6){
+        return "Paper";
+    } else {
+        return "Scissors"
+    }
+}
 
 // Take an imput
 
@@ -9,3 +18,5 @@
 // Count the score and
 
 // Print the winner
+
+console.log(computerPlay());
