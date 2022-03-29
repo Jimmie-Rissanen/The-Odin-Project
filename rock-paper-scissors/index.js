b9a8787b41c29e5bf43218955c45ca8787c6095e
Jimmie <jimmieriss@gmail.com>
2022-03-29 20:09:10 +0200

    The app works and looks like it should

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -6,25 +6,33 @@ const displayResultArea = document.querySelector('.displayResultArea');
 const gameStartButton = displayResultArea.querySelector('.gameStartButton');
 gameStartButton.addEventListener('click', startTheGame);
 
+const chooseArea = document.querySelector('.choose');
+
+const header = document.querySelector('.header');
+
+const headerText = document.querySelector('.heading');
+
 const scoreBoard = displayResultArea.querySelector('.scoreBoard');
 
 const yourScore = scoreBoard.querySelector('.yourScore').childNodes[3];
 const computerScore = scoreBoard.querySelector('.computerScore').childNodes[3];
 
-const winner = document.querySelector('.winner');
 const playAgainButton = document.querySelector('.playAgainButton');
 playAgainButton.addEventListener('click', newGame)
+
 
 // Start Game and show score
 
 function startTheGame(){
+    headerText.classList.toggle('hide');
+    mobileMoveButton();
     displayResult();
 }
 
 function displayResult(){
-    if(displayResultArea.contains(gameStartButton)){
-        displayResultArea.removeChild(gameStartButton);
-        scoreBoard.classList.remove('hide');
+    if(!gameStartButton.classList.contains('hide')){
+        gameStartButton.classList.toggle('hide');
+        scoreBoard.classList.toggle('hide');
     }
 }
 
@@ -37,23 +45,26 @@ function countScore(score){
 }
 
 function playRound (e) {
-    if(scoreBoard.classList.contains('hide') === false){
+    if(!gameStartButton.classList.contains('hide') || playAgainButton.classList.contains('hide')){
         const player = playerSelection(e);
         const computer = computerSelection();
         const result = evaluateWinner(player, computer);
+        flashResultColor(result);
         countScore(result);
         displayWinner();
     }
 }
 
 
-// Show the winner in a modal
+// Show the winner in a header 
 
 function displayWinner(){
     if(yourScore.innerText == 5){
+        mobileMoveButton();
         createWinnerDiv('You won!');
     } else if (computerScore.innerText == 5){
-        createWinnerDiv('The Lost!');
+        mobileMoveButton();
+        createWinnerDiv('You Lost!');
     }
 }
 
@@ -62,14 +73,15 @@ function createWinnerDiv(text){
     if(displayResultArea.contains(scoreBoard)){
         displayResultArea.removeChild(scoreBoard);
     }
-    winnerText.innerText = text;
-    displayResultArea.appendChild(winner);
-    winner.classList.remove('hide')
+    headerText.innerText = text;
+    headerText.classList.toggle('hide');
+    playAgainButton.classList.toggle('hide');
 }
 
 function newGame(){
-    displayResultArea.removeChild(winner);
-    winner.classList.add('hide')
+    playAgainButton.classList.toggle('hide');
+    headerText.classList.toggle('hide');
+    mobileMoveButton();
     yourScore.innerText = 0;
     computerScore.innerText = 0;
     displayResultArea.appendChild(scoreBoard);
@@ -107,4 +119,35 @@ function evaluateWinner(player, computer){
     }
 }
 
+// Utility functions
 
+function toggleHide(theElement){
+    theElement.classList.toggle('hide');
+}
+
+function mobileMoveButton(){
+    if(window.innerWidth <= 700){
+        chooseArea.classList.toggle('order');
+    }
+}
+
+function flashResultColor(result){
+    if(result === 0){
+       delayToggle('redFlash')
+    } else if(result === 1){
+       delayToggle('greenFlash') 
+    } else{
+       delayToggle('evenFlash')
+    }
+
+}
+
+function wait(milliSec){
+    return new Promise(resolve => setTimeout(resolve, milliSec));
+}
+
+async function delayToggle(className){
+    header.classList.toggle(className);
+    await wait(250);
+    header.classList.toggle(className);
+}
